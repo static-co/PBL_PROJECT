e5faf8afaaae8973045abf01bf643b8e00930ed5
unknown <businessstaticbolt@gmail.com>
2025-04-03 21:51:12 +0530

    comments

--- sorting/main.py
+++ sorting/main.py
@@ -13,36 +13,36 @@ import sorting_algorithms.timsort as timsort
 import sorting_algorithms.bogosort as bogosort
 
 sorting_list_dict = {
-    "bubblesort": ("list", bubblesort),
+    "bubblesort": ("list", bubblesort), #datatypes
     "selectionsort": ("list", selectionsort),
     "insertionsort": ("list", insertionsort),
     "mergesort": ("list", mergesort),
     "quicksort": ("list", quicksort),
     "heapsort": ("list", heapsort),
     "radixsort": ("list", radixsort),
-    "bucketsort": ("dict", bucketsort),
+    "bucketsort": ("dict", bucketsort), #this one requires a dict
     "timsort": ("list", timsort),
     "bogosort": ("list", bogosort)
 }
 
-print("Available Sorting Algorithms:")
+print("Available Sorting Algorithms:") #prints all the sorting algos
 for algo in sorting_list_dict:
     print(f"- {algo.capitalize()}")
 
-selected_algo = input("\nEnter sorting algorithm name: ").replace(" ", "").lower()
+selected_algo = input("\nEnter sorting algorithm name: ").replace(" ", "").lower() #input sorting algo to use
 
 if selected_algo not in sorting_list_dict:
     print("Invalid selection")
 else:
-    data_type, module = sorting_list_dict[selected_algo]
+    data_type, module = sorting_list_dict[selected_algo] #chooses data structure and algo
 
-    data_structure = [] if data_type == "list" else {}
+    data_structure = [] if data_type == "list" else {} #else dict
 
     print(f"\nYou selected: {selected_algo.capitalize()}")
     print(f"Initialized data structure: {type(data_structure).__name__}")
 
     while True:
-        try:
+        try: #no of elements in space
             search_space_size = int(input("Enter the number of elements for the search space: "))
             if search_space_size <= 0:
                 print("Size must be a positive integer.")
@@ -51,12 +51,12 @@ else:
         except ValueError:
             print("Invalid input. Please enter a valid number.")
 
-    if isinstance(data_structure, list):
-        data_structure = [random.randint(1, 100) for _ in range(search_space_size)]
-    else:
+    if isinstance(data_structure, list): #if list
+        data_structure = [random.randint(1, 100) for _ in range(search_space_size)] #random numbers between 1 to 100
+    else: #if dict
         data_structure = {i: random.randint(1, 100) for i in range(search_space_size)}
 
     print(f"\nGenerated Search Space of {search_space_size} elements.")
 
     # Call the sorting function
-    module.sort(data_structure)
+    module.sort(data_structure) # this calls the functions in the other file
